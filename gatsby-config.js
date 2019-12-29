3c4e9911adbe19744d96111ded0a8a18e391a742
Emma Bostian <emmabostian@Emmas-MBP.fritz.box>
2019-12-29 17:59:28 +0100

    Setting up app

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Ladybug Podcast`,
+    description: `Emma Bostian, Kelly Vaughn, and Ali Spittel talk career and code.`,
+    author: `@emmabostian`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -24,7 +24,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/ladybug@2x.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
