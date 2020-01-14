df4d5107a55e4f96ddd04627b4dc6b965cf1a0d5
Nifty Marketing <johnathan@niftymarketing.com>
2020-01-13 20:03:15 -0700

    removed hover state, master merge

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,92 +8,95 @@ import itunes from "../images/footer/itunes.svg"
 import podcast from "../images/footer/podcast.svg"
 import "./footer.css"
 
-const Footer = () => (
-  <footer className="footer">
-    <section className="footer-find-episodes">
-      <p>
-        <span role="img" aria-label="Headphones">
-          🎧
-        </span>{" "}
-        Find our episodes where you listen to podcasts{" "}
-        <span role="img" aria-label="Headphones">
-          🎧
-        </span>
+const Footer = () => {
+  const currentYear = new Date().getFullYear()
+
+  return (
+    <footer className="footer">
+      <section className="footer-find-episodes">
+        <p>
+          <span role="img" aria-label="Headphones">
+            🎧
+          </span>{" "}
+          Find our episodes where you listen to podcasts{" "}
+          <span role="img" aria-label="Headphones">
+            🎧
+          </span>
+        </p>
+        <address className="footer-icons">
+          <a
+            href="https://twitter.com/LadybugPodcast"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={twitter} alt="Twitter" />
+          </a>
+          <a
+            href="https://play.google.com/music/listen?u=0#/ps/I4lyvappbarpo5olinvd2gxh3km"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={google} alt="Google Play" />
+          </a>
+          <a
+            href="https://open.spotify.com/show/1KBO1tSnm0XRlEILmqt7Em"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={spotify} alt="Spotify" />
+          </a>
+          <a
+            href="https://podcasts.apple.com/us/podcast/ladybug-podcast/id1469229625"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={itunes} alt="Apple Podcasts" />
+          </a>
+          <a
+            href="https://www.stitcher.com/podcast/ladybug-podcast"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={podcast} alt="Stitcher" />
+          </a>
+          <a
+            href="https://pinecast.com/feed/ladybug-podcast"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={rss} alt="RSS Feed" />
+          </a>
+        </address>
+      </section>
+      <div className="footer-links">
+        <Link to="/contact">Sponsor Us</Link>
+        <a
+          href="https://github.com/ladybug-podcast/ladybug-website"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Fix Our Website
+        </a>
+        <a
+          href="https://docs.google.com/forms/d/e/1FAIpQLSc0IBTZA1Acv9a05VQAjtSgv-M8GRtGq7yDXSKL_vYKGcBlTw/viewform"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Nominate A Guest
+        </a>
+        <a
+          href="https://forms.gle/UDVfz2cu73UyGhieA"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Request An Episode
+        </a>
+      </div>
+      <p className="footer-copyright">
+        {`© Ladybug Podcast ${currentYear}. Produced by Kelly Vaughn, Ali Spittel, and Emma Bostian.`}
       </p>
-      <address className="footer-icons">
-        <a
-          href="https://twitter.com/LadybugPodcast"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src={twitter} alt="Twitter" />
-        </a>
-        <a
-          href="https://play.google.com/music/listen?u=0#/ps/I4lyvappbarpo5olinvd2gxh3km"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src={google} alt="Google Play" />
-        </a>
-        <a
-          href="https://open.spotify.com/show/1KBO1tSnm0XRlEILmqt7Em"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src={spotify} alt="Spotify" />
-        </a>
-        <a
-          href="https://podcasts.apple.com/us/podcast/ladybug-podcast/id1469229625"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src={itunes} alt="Apple Podcasts" />
-        </a>
-        <a
-          href="https://www.stitcher.com/podcast/ladybug-podcast"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src={podcast} alt="Stitcher" />
-        </a>
-        <a
-          href="https://pinecast.com/feed/ladybug-podcast"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src={rss} alt="RSS Feed" />
-        </a>
-      </address>
-    </section>
-    <div className="footer-links">
-      <Link to="/contact">Sponsor Us</Link>
-      <a
-        href="https://github.com/ladybug-podcast/ladybug-website"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Fix Our Website
-      </a>
-      <a
-        href="https://docs.google.com/forms/d/e/1FAIpQLSc0IBTZA1Acv9a05VQAjtSgv-M8GRtGq7yDXSKL_vYKGcBlTw/viewform"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Nominate A Guest
-      </a>
-      <a
-        href="https://forms.gle/UDVfz2cu73UyGhieA"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Request An Episode
-      </a>
-    </div>
-    <p className="footer-copyright">
-      © Ladybug Podcast 2019. Produced by Kelly Vaughn, Ali Spittel, and Emma
-      Bostian.
-    </p>
-  </footer>
-)
+    </footer>
+  )
+}
 
 export default Footer
