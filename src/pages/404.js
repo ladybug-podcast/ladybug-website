649c058a7a87836f566c2b4ab51027ef1de10f7e
Flavia Nunes <flavianunes@decom.ufop.br>
2020-04-08 09:14:49 -0300

    Adding custom 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,21 @@
 import React from "react"
-
+import { graphql } from 'gatsby';
+import Img from "gatsby-image"
 import SEO from "../components/seo"
+import PageHeader from "../components/PageHeader"
+import Footer from "../components/Footer"
+import "./pages.css"
 
 const NotFoundPage = () => (
-  <div>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+  <div className="page">
+    <SEO title="404: Page not found" />
+    <PageHeader />
+    <main>
+      <div className="page-banner">
+        <h1>404: Page not found</h1>
+      </div>
+    </main>
+    <Footer />
   </div>
 )
 
